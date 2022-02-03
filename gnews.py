7cdc6595193e31f63ab97a64eaf4fb9673f8ab5c
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2022-02-03 14:10:46 +0530

    Add files via upload

--- gnews.py
+++ gnews.py
@@ -8,12 +8,25 @@ st.title("Live News")
 country=st.text_input("Enter the Country or city Name ??")
 if st.button('SEARCH'):
     go = GoogleNews(period='7d')
+    go.setlang("hindi")
     go.search(country)
+
     result = go.result()
     for x in result:
         st.write("-"*40)
-        st.write(x['title'])
-        st.write(x['date'])
-        st.write(x['desc'])
+        st.markdown(
+            f"<h5 style='text-align:justify;color:teal;'><b><u>Title---</u></b><br>{x['title']}</h5>",unsafe_allow_html=True)
+        st.markdown(
+            f"<h5 style='text-align:justify;color:black;'><b><u>Date and Time---</u></b><br>{x['date']}</h5>", unsafe_allow_html=True)
+        st.markdown(
+            f"<h5 style='text-align:justify;color:red;'><b><u>Description---</u></b><br>{x['desc']}</h5>", unsafe_allow_html=True)
         st.write(x['link'])
+        # #st.markdown(f"'<h5 style='text-align:justify;color:black;'>{x['media']}</h5", unsafe_allow_html=True)
+
+        #st.write(x['title'])
+        #st.write(x['date'])
+        #st.write(x['desc'])
+        #imh=x['img']
+        #st.image(imh)
+        st.write(x['media'])
     st.markdown("'<h4 style='text-align:center;color:teal;'>This Project is made by Ayush Arya<br>@kabir_arya001</h4>",unsafe_allow_html=True)
